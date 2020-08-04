d43b3496fbc9ef87ce8cf0ceeda3602982e4523b
chtelmko <katerynachtel@gmail.com>
2020-08-04 16:11:40 +0000

    create and show customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,24 @@
 class CustomersController < ApplicationController
+    def index
+        @customers = Customer.all
+    end
+    
     def new
     end
     
     def create
+        @customer = Customer.new(customer_params)
+ 
+        @customer.save
+        redirect_to @customer
     end
-end
+    
+    def show
+        @customer = Customer.find(params[:id])
+    end
+ 
+    private
+      def customer_params
+        params.require(:customer).permit(:email)
+      end
+    end
