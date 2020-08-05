c5dac370f507ac7e6476ed414ea06fb63855fffd
chtelmko <katerynachtel@gmail.com>
2020-08-05 01:53:58 +0000

    Add links back to the index page, for orders and customers views

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  
+   validates :city, presence: true,
+                    length: { minimum: 5 }
+    validates :street_address, presence: true,
+                    length: { minimum: 10 }                    
+    validates :state, presence: true,
+                    length: { minimum: 2 }
+    validates :postal_code, presence: true,
+                    length: { minimum: 5 } 
+    validates :ship_date, presence: true
 end
